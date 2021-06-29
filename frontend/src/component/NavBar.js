ffe8546e59a6a418e5e4a8fc8f87aef9d9e2fa83
Ayyoub Halbaoui <57773423+ayyoubhalbaoui1@users.noreply.github.com>
2021-06-29 18:19:12 +0100

    ok
    Some css

--- frontend/src/component/NavBar.js
+++ frontend/src/component/NavBar.js
@@ -3,19 +3,14 @@ import React from 'react';
 
 export default function NavBar () {
     return(
-        <nav class="navbar">
-            <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse">
-                <i class="fas fa-bars fa-fw"></i>
-                </button>
-            </div>
+        <nav  class="navbar">
             <div class="collapse navbar-collapse">
             <ul class="nav navbar-nav navbar-right">
                 <li>
-                <a class="navbar-brand" href="/">Home</a>
+                <a class="navbar-brand" style={{color: "Red"}} href="/">Home</a>
                 </li>
                 <li>
-                <a class="navbar-brand" href="/Commands">Reservation List</a>
+                <a class="navbar-brand"style={{color: "red"}} href="/Commands">Reservation List</a>
                 </li>
             </ul>
             </div>
